77cad75fc5fcbf313e1f2fe7f49e1936fda17c16
MartonLyra <marton2050@gmail.com>
2023-04-13 19:13:10 -0300

    Código gerado pelo Spring Initializr.
    Bibliotecas sendo usadas:
    
    - Spring Web
    - Spring Data JPA
    - PostgreSQL Driver
    - Validation
    - Lombok
    
    MartonJr.

---
+++ src/main/java/me/dio/desafioacademiadigital/DesafioAcademiaDigitalApplication.java
@@ -0,0 +1,13 @@
+package me.dio.desafioacademiadigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioAcademiaDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioAcademiaDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/desafioacademiadigital/DesafioAcademiaDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.desafioacademiadigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioAcademiaDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
